67337260c318126d083fbbc9948e94e7d5476ee8
Belle Wittams-Nangle <owittnan+git@gmail.com>
2025-05-20 17:18:24 +0000

    statuscolors patch

--- config.def.h
+++ config.def.h
@@ -18,10 +18,18 @@ static char normfgcolor[]           = "#bbbbbb";
 static char selfgcolor[]            = "#eeeeee";
 static char selbordercolor[]        = "#005577";
 static char selbgcolor[]            = "#005577";
+static const char col_black[]       = "#000000";
+static const char col_red[]         = "#ff0000";
+static const char col_yellow[]      = "#ffff00";
+static const char col_white[]       = "#ffffff";
+
 static char *colors[][3] = {
-       /*               fg           bg           border   */
-       [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
-       [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
+       /*                 fg           bg           border   */
+       [SchemeNorm]   = { normfgcolor, normbgcolor, normbordercolor },
+       [SchemeSel]    = { selfgcolor,  selbgcolor,  selbordercolor  },
+	   [SchemeWarn]   = { col_black,   col_yellow,  col_red },
+	   [SchemeUrgent] = { col_white,   col_red,     col_red}
+	   
 };
 
 /* tagging */
