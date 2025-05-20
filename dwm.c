2067f1ec563fd0f974bfb908b7f4a92ecab63092
Belle Wittams-Nangle <owittnan+git@gmail.com>
2025-05-20 17:06:14 +0000

    restartsig

--- dwm.c
+++ dwm.c
@@ -205,6 +205,8 @@ static void setup(void);
 static void seturgent(Client *c, int urg);
 static void showhide(Client *c);
 static void spawn(const Arg *arg);
+static void sighup(int unused);
+static void sigterm(int unused);
 static void tag(const Arg *arg);
 static void tagmon(const Arg *arg);
 static void tile(Monitor *m);
@@ -259,6 +261,7 @@ static void (*handler[LASTEvent]) (XEvent *) = {
 	[UnmapNotify] = unmapnotify
 };
 static Atom wmatom[WMLast], netatom[NetLast];
+static int restart = 0;
 static int running = 1;
 static Cur *cursor[CurLast];
 static Clr **scheme;
@@ -1257,6 +1260,7 @@ propertynotify(XEvent *e)
 void
 quit(const Arg *arg)
 {
+	if(arg->i) restart = 1;
 	running = 0;
 }
 
@@ -1549,6 +1553,9 @@ setup(void)
 	sa.sa_handler = SIG_IGN;
 	sigaction(SIGCHLD, &sa, NULL);
 
+	signal(SIGTERM, sigterm);
+	signal(SIGHUP, sighup);
+
 	/* clean up any zombies (inherited from .xinitrc etc) immediately */
 	while (waitpid(-1, NULL, WNOHANG) > 0);
 
@@ -1610,6 +1617,16 @@ setup(void)
 	XSelectInput(dpy, root, wa.event_mask);
 	grabkeys();
 	focus(NULL);
+}
+
+void sighup(int unused) {
+	Arg a = {.i = 1};
+	quit(&a);
+}
+
+void sigterm(int unused) {
+	Arg a = {.i = 0};
+	quit(&a);
 }
 
 void
@@ -2158,6 +2175,7 @@ main(int argc, char *argv[])
 #endif /* __OpenBSD__ */
 	scan();
 	run();
+	if (restart) execvp(argv[0], argv);
 	cleanup();
 	XCloseDisplay(dpy);
 	return EXIT_SUCCESS;
